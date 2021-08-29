dc2753543510acca9ed3291bab4c67be2d8e0cc8
Noah Luisi-Juricic <noah@Noahs-MBP.modem>
2021-08-30 05:51:52 +1000

    Feature hide is done

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public class PrintAndHide : MonoBehaviour
 {
     private const string x = ":";
-    private int i = 3;
+    int i, randValue;
+   
     public Renderer rend;
     // Start is called before the first frame update
     void Start()
     {
-         
+        i = 3;
+        randValue = Random.Range(200, 251);
     }
 
     // Update is called once per frame
@@ -19,7 +21,10 @@ public class PrintAndHide : MonoBehaviour
         
         i++;
         Debug.Log(gameObject.name + x + i);
-        
-       
+
+        if (gameObject.CompareTag("Red") && i == 100)
+            gameObject.SetActive(false);
+        if (gameObject.CompareTag("Blue") && i == randValue)
+            rend.enabled = false;
     }
 }
